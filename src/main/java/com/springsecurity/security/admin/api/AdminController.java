23cf65089700d2837e957f235ff5f6d678b2be78
jungu0416 <jungu0416@naver.com>
2023-06-07 13:45:47 +0900

    도메인 패키징2

--- src/main/java/com/springsecurity/security/admin/api/AdminController.java
+++ src/main/java/com/springsecurity/security/admin/api/AdminController.java
@@ -1,16 +1,16 @@
-package com.springsecurity.security.board.api;
+package com.springsecurity.security.admin.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/board")
-public class BoardController {
+@RequestMapping("/admin")
+public class AdminController {
 
-    @GetMapping("/dashboard")
-    public String dashboard() {
-        return "dashboard";
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
     }
 
 }
